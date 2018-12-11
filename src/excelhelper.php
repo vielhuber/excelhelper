cd745e5bea4ce6f6e589636bbb1c43195f64258f
David Vielhuber <david@vielhuber.de>
2018-12-11 15:16:39 +0100

    Feature completed

--- src/excelhelper.php
+++ src/excelhelper.php
@@ -1,17 +1,215 @@
 <?php
 namespace vielhuber\excelhelper;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
 class excelhelper
 {
-
-    private $session = null;
-
-    public function __construct()
+    public static function read($args)
     {
+        $args = self::prepareReadArgs($args);
+
+        $array = [];
+        $filetype = IOFactory::identify($args['file']);
+        $reader = IOFactory::createReader($filetype);
+        $spreadsheet = $reader->load($args['file']);
+
+        if ($args['all_sheets'] === false) {
+            $sheet = $spreadsheet->getSheet(0);
+            $array = $sheet->toArray(null, true, $args['format_cells']);
+            if ($args['first_line'] === false) {
+                unset($array[0]);
+                $array = array_values($array);
+            }
+        } else {
+            foreach ($spreadsheet->getWorksheetIterator() as $sheet_nr => $sheet) {
+                $array_part = $sheet->toArray(null, true, $args['format_cells']);
+                if ($args['first_line'] === false) {
+                    unset($array_part[0]);
+                    $array_part = array_values($array_part);
+                }
+                $array = array_merge($array, $array_part);
+            }
+        }
+        return $array;
     }
 
-    public function add($content)
+    private static function prepareReadArgs($args)
     {
+        // default values
+        if (!array_key_exists('first_line', $args)) {
+            $args['first_line'] = true;
+        }
+        if (!array_key_exists('format_cells', $args)) {
+            $args['format_cells'] = false;
+        }
+        if (!array_key_exists('all_sheets', $args)) {
+            $args['all_sheets'] = false;
+        }
+        // checks
+        if (!isset($args['file']) && !file_exists($args['file'])) {
+            throw new \Exception('file missing');
+        }
+        if (!in_array($args['first_line'], [true, false])) {
+            throw new \Exception('unknown first_line');
+        }
+        if (!in_array($args['format_cells'], [true, false])) {
+            throw new \Exception('unknown format_cells');
+        }
+        if (!in_array($args['all_sheets'], [true, false])) {
+            throw new \Exception('unknown all_sheets');
+        }
+        return $args;
     }
 
+    public static function write($args)
+    {
+        $args = self::prepareWriteArgs($args);
+        if ($args['engine'] === 'phpspreadsheet') {
+            $spreadsheet = new Spreadsheet();
+            $sheet = $spreadsheet->getActiveSheet();
+            foreach ($args['data'] as $data__key => $data__value) {
+                $row = $data__key + 1;
+                foreach ($data__value as $data__value__key => $data__value__value) {
+                    $col = self::int2char($data__value__key + 1);
+                    if (!is_array($data__value__value)) {
+                        $sheet->setCellValue($col . $row, $data__value__value);
+                    } else {
+                        if (array_key_exists('value', $data__value__value)) {
+                            $sheet->setCellValue($col . $row, $data__value__value['value']);
+                        }
+                        if (array_key_exists('background-color', $data__value__value)) {
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getFill()
+                                ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getFill()
+                                ->getStartColor()
+                                ->setARGB('00' . str_replace('#', '', $data__value__value['background-color']));
+                        }
+                        if (array_key_exists('color', $data__value__value)) {
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getFont()
+                                ->getColor()
+                                ->setARGB('00' . str_replace('#', '', $data__value__value['color']));
+                        }
+                        if (array_key_exists('font-weight', $data__value__value) && $data__value__value['font-weight'] === 'bold') {
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getFont()
+                                ->setBold(true);
+                        }
+                        if (array_key_exists('text-align', $data__value__value) && $data__value__value['text-align'] === 'center') {
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getAlignment()
+                                ->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                        }
+                        if (array_key_exists('text-align', $data__value__value) && $data__value__value['text-align'] === 'right') {
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getAlignment()
+                                ->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+                        }
+                        if (array_key_exists('border', $data__value__value)) {
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getBorders()
+                                ->getTop()
+                                ->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getBorders()
+                                ->getLeft()
+                                ->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getBorders()
+                                ->getBottom()
+                                ->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                            $sheet
+                                ->getStyle($col . $row)
+                                ->getBorders()
+                                ->getRight()
+                                ->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                        }
+                    }
+                }
+            }
+            $writer = new Xlsx($spreadsheet);
+            if ($args['output'] === 'save') {
+                $writer->save($args['file']);
+            }
+            if ($args['output'] === 'download') {
+                header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                header('Content-Disposition: attachment;filename="' . $args['file'] . '"');
+                header('Cache-Control: max-age=0');
+                $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+                $writer->save('php://output');
+            }
+        }
+        if ($args['engine'] === 'spout') {
+        }
+    }
+
+    private static function prepareWriteArgs($args)
+    {
+        // default values
+        if (!array_key_exists('file', $args)) {
+            $args['file'] = 'output-' . date('Y-m-d-H:i:s') . '.xlsx';
+        }
+        if (!array_key_exists('engine', $args)) {
+            $args['engine'] = 'phpspreadsheet';
+        }
+        if (!array_key_exists('output', $args)) {
+            $args['output'] = 'save';
+        }
+        if (!array_key_exists('data', $args)) {
+            $args['data'] = [];
+        }
+        // checks
+        if (self::getType($args['file']) === null) {
+            throw new \Exception('unknown file extension');
+        }
+        if (!in_array($args['engine'], ['phpspreadsheet', 'spout'])) {
+            throw new \Exception('unknown engine');
+        }
+        if (!in_array($args['output'], ['save', 'download'])) {
+            throw new \Exception('unknown output');
+        }
+        return $args;
+    }
+
+    private static function getType($file)
+    {
+        if (strpos($file, '.xlsx') === strlen($file) - strlen('.xlsx')) {
+            return 'xlsx';
+        }
+        return null;
+    }
+
+    private static function char2int($letters)
+    {
+        $num = 0;
+        $arr = array_reverse(str_split($letters));
+        for ($i = 0; $i < count($arr); $i++) {
+            $num += (ord(strtolower($arr[$i])) - 96) * pow(26, $i);
+        }
+        return $num;
+    }
+    private static function int2char($num)
+    {
+        $letters = '';
+        while ($num > 0) {
+            $code = $num % 26 == 0 ? 26 : $num % 26;
+            $letters .= chr($code + 64);
+            $num = ($num - $code) / 26;
+        }
+        return strtoupper(strrev($letters));
+    }
 }
--- tests/Test.php
+++ tests/Test.php
@@ -3,10 +3,28 @@ use vielhuber\excelhelper\excelhelper;
 
 class Test extends \PHPUnit\Framework\TestCase
 {
-  
     function test__excelhelper()
     {
-
+        excelhelper::write([
+            'file' => 'tests/test.xlsx',
+            'engine' => 'phpspreadsheet',
+            'output' => 'save',
+            'data' => [
+                ['a1', 'b1', 'c1'],
+                ['a2', 'b2', 'c2'],
+                [
+                    ['value' => 'a3', 'background-color' => '#ff0000', 'color' => '#ffffff', 'font-weight' => 'bold', 'border' => '1px solid #000', 'text-align' => 'center'],
+                    ['value' => 'b3', 'background-color' => '#ff0000', 'color' => '#ffffff', 'font-weight' => 'bold', 'border' => '1px solid #000', 'text-align' => 'left'],
+                    ['value' => 'c3', 'background-color' => '#ff0000', 'color' => '#ffffff', 'font-weight' => 'bold', 'border' => '1px solid #000', 'text-align' => 'right']
+                ]
+            ]
+        ]);
+        $array = excelhelper::read([
+            'file' => 'tests/test.xlsx',
+            'first_line' => true,
+            'format_cells' => false,
+            'all_sheets' => false
+        ]);
+        $this->assertEquals($array, [['a1', 'b1', 'c1'], ['a2', 'b2', 'c2'], ['a3', 'b3', 'c3']]);
     }
-
 }
