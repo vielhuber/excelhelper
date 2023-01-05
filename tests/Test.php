fa311acb4e83c600e49de17bc431d7d4495e5552
David Vielhuber <david@vielhuber.de>
2023-01-05 23:27:53 +0100

    Improve github actions.

--- tests/Test.php
+++ tests/Test.php
@@ -112,5 +112,6 @@ class Test extends \PHPUnit\Framework\TestCase
             'bar' => [['d', 'e', 'f']],
             'baz' => [['g', 'h', 'i']]
         ]);
+        $this->assertEquals(true, false);
     }
 }
